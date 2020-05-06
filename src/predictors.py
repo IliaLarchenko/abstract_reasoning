60f11b7222eb95f1e2f3e50431a41ac9fc7dfb0c
Ilia <ilia.larchenko@gmail.com>
2020-05-06 21:06:50 +0300

    added fill outer class

--- src/predictors.py
+++ src/predictors.py
@@ -17,10 +17,6 @@ class predictor:
                 new_params[k] = v
         return new_params
 
-    def predict_output(self, image, params, color_scheme, retrive_params_values):
-        """ predicts 1 output image given input image and prediction params"""
-        return 1, None
-
     def get_images(self, k, train=True):
         if train:
             original_image = np.uint8(self.sample["train"][k]["input"])
@@ -29,6 +25,10 @@ class predictor:
         else:
             original_image = np.uint8(self.sample["test"][k]["input"])
             return original_image
+
+    def predict_output(self, image, params):
+        """ predicts 1 output image given input image and prediction params"""
+        return 1, None
 
     def process_one_sample(self, k, initial=False):
         """ processes k train sample and updates self.solution_candidates"""
@@ -74,3 +74,61 @@ class predictor:
             return 0, answers
         else:
             return 3, None
+
+
+class fill_outer(predictor):
+    def predict_output(self, image, params):
+        """ predicts 1 output image given input image and prediction params"""
+        result = image.copy()
+        for i in range(1, image.shape[0] - 1):
+            for j in range(1, image.shape[1] - 1):
+                if image[i, j] == params["fill_color"]:
+                    result[i - 1 : i + 2, j - 1 : j + 2][
+                        np.array(
+                            [
+                                [True, True, True],
+                                [True, False, True],
+                                [True, True, True],
+                            ]
+                        )
+                    ] = params["background_color"]
+
+        return 0, result
+
+    def process_one_sample(self, k, initial=False):
+        """ processes k train sample and updates self.solution_candidates"""
+        local_candidates = []
+        original_image, target_image = self.get_images(k)
+        if original_image.shape != target_image.shape:
+            return 1, None
+        for background_color in range(10):
+            if not (target_image == background_color).any():
+                continue
+            for fill_color in range(10):
+                if not (target_image == fill_color).any():
+                    continue
+                if not (target_image == original_image)[
+                    np.logical_and(
+                        target_image != background_color, target_image != fill_color
+                    )
+                ].all():
+                    continue
+                params = {
+                    "background_color": background_color,
+                    "fill_color": fill_color,
+                }
+                prediction = self.predict_output(original_image, params)
+                if (prediction == target_image).all():
+                    for color_fill_dict in self.sample["processed_train"][k]["colors"][
+                        fill_color
+                    ]:
+                        for color_background_dict in self.sample["processed_train"][k][
+                            "colors"
+                        ][background_color]:
+                            self.solution_candidates.append(
+                                {
+                                    "fill_color": color_fill_dict,
+                                    "background_color": color_background_dict,
+                                }
+                            )
+        return 0
