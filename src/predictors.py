0c8408b35f5aef041ac1894fe8fe4414d8e65e34
Ilia <ilia.larchenko@gmail.com>
2020-05-06 21:45:27 +0300

    fixed bugs in classes

--- src/predictors.py
+++ src/predictors.py
@@ -1,9 +1,10 @@
 import numpy as np
 from src.preprocessing import get_color, get_color_scheme
+from src.functions import filter_list_of_dicts
 
 
 class predictor:
-    def init(self, params, preprocess_params):
+    def init(self, params=None, preprocess_params=None):
         self.params = params
         self.preprocess_params = preprocess_params
         self.solution_candidates = []
@@ -13,9 +14,11 @@ class predictor:
         for k, v in params.items():
             if k[-5:] == "color":
                 new_params[k] = get_color(v, color_scheme["colors"])
+                if new_params[k] < 0:
+                    return 1, None
             else:
                 new_params[k] = v
-        return new_params
+        return 0, new_params
 
     def get_images(self, k, train=True):
         if train:
@@ -38,7 +41,7 @@ class predictor:
 
     def process_full_train(self):
         for k in range(len(self.sample["train"])):
-            status = self.process_one_sample(self, k, initial=(k == 0))
+            status = self.process_one_sample(k, initial=(k == 0))
             if status != 0:
                 return 1
 
@@ -47,7 +50,9 @@ class predictor:
 
         return 0
 
-    def __call__(self):
+    def __call__(self, sample):
+        """ works like fit_predict"""
+        self.sample = sample
         status = self.process_full_train()
         if status != 0:
             return status, None
@@ -58,15 +63,16 @@ class predictor:
 
         result_generated = False
         for test_n, test_data in enumerate(self.sample["test"]):
-            original_image = self.get_images(self, test_n, train=False)
+            original_image = self.get_images(test_n, train=False)
             color_scheme = get_color_scheme(original_image)
             for params_dict in self.solution_candidates:
                 status, params = self.retrive_params_values(params_dict, color_scheme)
                 if status != 0:
                     continue
-                status, prediction = self.predict_output(original_image, color_scheme)
+                status, prediction = self.predict_output(original_image, params)
                 if status != 0:
                     continue
+
                 answers[test_n].append(prediction)
                 result_generated = True
 
@@ -78,6 +84,9 @@ class predictor:
 
 class fill_outer(predictor):
     """fill all pixels around all pixels with particular color with new color"""
+
+    def init(self, params=None, preprocess_params=None):
+        super().__init__(params, preprocess_params)
 
     def predict_output(self, image, params):
         """ predicts 1 output image given input image and prediction params"""
@@ -102,24 +111,25 @@ class fill_outer(predictor):
         local_candidates = []
         original_image, target_image = self.get_images(k)
         if original_image.shape != target_image.shape:
-            return 1, None
+            return 4, None
         for background_color in range(10):
             if not (target_image == background_color).any():
                 continue
             for fill_color in range(10):
                 if not (target_image == fill_color).any():
                     continue
-                if not (target_image == original_image)[
-                    np.logical_and(
-                        target_image != background_color, target_image != fill_color
-                    )
-                ].all():
+                mask = np.logical_and(
+                    target_image != background_color, target_image != fill_color
+                )
+                if not (target_image == original_image)[mask].all():
                     continue
                 params = {
                     "background_color": background_color,
                     "fill_color": fill_color,
                 }
-                prediction = self.predict_output(original_image, params)
+                status, prediction = self.predict_output(original_image, params)
+                if status != 0:
+                    continue
                 if (prediction == target_image).all():
                     for color_fill_dict in self.sample["processed_train"][k]["colors"][
                         fill_color
@@ -127,10 +137,19 @@ class fill_outer(predictor):
                         for color_background_dict in self.sample["processed_train"][k][
                             "colors"
                         ][background_color]:
-                            self.solution_candidates.append(
+                            local_candidates.append(
                                 {
                                     "fill_color": color_fill_dict,
                                     "background_color": color_background_dict,
                                 }
                             )
-        return 0
+        if initial:
+            self.solution_candidates = local_candidates
+        else:
+            self.solution_candidates = filter_list_of_dicts(
+                local_candidates, self.solution_candidates
+            )
+        if len(self.solution_candidates) == 0:
+            return 5
+        else:
+            return 0
