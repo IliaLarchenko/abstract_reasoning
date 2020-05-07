65d4bb88cb056a3de6af4e54272fba34b931cfad
Ilia <ilia.larchenko@gmail.com>
2020-05-08 02:44:41 +0300

    added pattern predictor class tests

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -28,8 +28,8 @@ def check(predictor_class, params, file_path, DATA_PATH, preprocessing_params):
 
 def test_predictor():
     for id, predictor_class, params, file_path, DATA_PATH, preprocessing_params in [
-        (1, fill, {"type": "outer"}, "4258a5f9.json", "data/training", None),
-        (2, fill, {"type": "inner"}, "bb43febb.json", "data/training", None),
+        (1, fill, {"type": "outer"}, "4258a5f9.json", "data/training", ["initial"]),
+        (2, fill, {"type": "inner"}, "bb43febb.json", "data/training", ["initial"]),
         (3, puzzle, {"intersection": 0}, "a416b8f3.json", "data/training", None),
         (4, puzzle, {"intersection": 0}, "59341089.json", "data/evaluation", None),
         (5, puzzle, {"intersection": 0}, "25ff71a9.json", "data/training", None),
@@ -42,6 +42,8 @@ def test_predictor():
             "data/evaluation",
             ["initial", "min_max_blocks", "rotate", "cut_edges", "resize"],
         ),
+        (8, pattern, None, "ad7e01d0.json", "data/evaluation", ["initial"]),
+        (9, pattern, None, "5b6cbef5.json", "data/evaluation", ["initial"]),
     ]:
         assert (
             check(predictor_class, params, file_path, DATA_PATH, preprocessing_params)
